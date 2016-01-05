47fe218821989590845c6737feb3c4ad38e8b8ec
zdivozzo <zacharydivozzo@gmail.com>
2016-01-04 19:24:15 -0500

    Github pages

--- src/components/run.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import store from '../store'
-import Container from './Main';
-
-// Render the main component into the dom
-ReactDOM.render(<Provider store={store}><Container /></Provider>, document.getElementById('app'));
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore,applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import actions from './reducers/answer'
-
-const createStoreWithMiddleware = applyMiddleware(
-  thunk
-)(createStore)
-
-export default createStoreWithMiddleware(actions)
